13ec6ca402faa477b941b10738ce055830e2c719
minjaer <550151@my.cuhsd.org>
2020-10-16 18:59:22 +0000

    I added the axes

---
+++ main.py
@@ -0,0 +1,29 @@
+import turtle as trtl
+
+# draws a curve left 90 degrees
+trtl.pendown()
+for i in range(6):
+  trtl.forward(10)
+  trtl.left(15)
+trtl.penup()
+
+""""
+draws the x- and y- axes 100 pixels each cardinal direction
+"""
+def draw_axes():
+  # centers at origin
+  trtl.goto(0,0)
+  trtl.pendown()
+  # draws the y-axis
+  trtl.forward(100)
+  trtl.backward(200)
+  trtl.forward(100)
+  trtl.left(90)
+  # draws the x-axis
+  trtl.forward(100)
+  trtl.backward(200)
+  # centers at origin
+  trtl.penup()
+  trtl.goto(0,0)
+
+draw_axes()
